1ae7985bde8b5de5206c2050e6d18e6c2503d628
Julian Vennen <julian@aternos.org>
2025-02-11 14:51:22 +0100

    Use shadowJar, relocate slf4j

--- bukkit/src/main/java/gs/mclo/bukkit/MclogsPlugin.java
+++ bukkit/src/main/java/gs/mclo/bukkit/MclogsPlugin.java
@@ -1,6 +1,12 @@
 package gs.mclo.bukkit;
 
+import gs.mclo.Constants;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MclogsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Constants.LOG.info("Using SLF4J");
+        getLogger().info("Using bukkit logger");
+    }
 }
